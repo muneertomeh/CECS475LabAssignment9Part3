4aa8baeb36c104f5ad64c9477066d7c6ce2d4567
mtome <mtome@MT-GL>
2018-12-18 21:51:51 -0800

    First commit

---
+++ CECS475LabAssignment9Part3.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using CECS475LabAssignment9Part3.Controllers;
+using CECS475LabAssignment9Part3.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CECS475LabAssignment9Part3.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            EmployeeController empController = new EmployeeController();
+            ViewResult viewResult = empController.Index() as ViewResult;
+            Assert.IsNotNull(viewResult);
+        }
+        [TestMethod]
+        public void TestEmployeesList()
+        {
+            EmployeeController empController = new EmployeeController();
+            IList<Employee> employeesList = empController.GetEmployees() as IList<Employee>;
+          
+        }
+
+        [TestMethod]
+        public void TestEmployees()
+        {
+            EmployeeController empController = new EmployeeController();
+            ViewResult theResult = empController.Employees() as ViewResult;
+            Assert.IsNotNull(theResult);
+        }
+
+
+    }
+}
---
+++ CECS475LabAssignment9Part3/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using CECS475LabAssignment9Part3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CECS475LabAssignment9Part3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employeeList = new List<Employee>
+            {
+                new Employee
+                {
+                    ID = 1,
+                    Name= "Brian Nguyen",
+                    JoiningDate = DateTime.Now,
+                    Age = 21
+                },
+                new Employee
+                {
+                    ID =2,
+                    Name="Brian Smith",
+                    JoiningDate = DateTime.Now.ToUniversalTime(),
+                    Age = 25
+                },
+            };
+
+            return employeeList;
+        }
+        public ActionResult Employees()
+        {
+            var employeeList = from e in this.GetEmployees() select e;
+            return View(employeeList);
+        }
+    }
+}
